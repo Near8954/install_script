9a1ba0629ab33edd4e8ed3e14343b908d1a6df2c
Near8954 <roma.baldin@gmail.com>
2024-03-25 16:34:32 +0300

    packages update

--- source/packages.sh
+++ source/packages.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo pacman -S obsidian tor torbrowser-launcher rnote flatpak sqlite zsh telegram-desktop kitty github-cli gnome-disk-utility binwalk firefox gnome-terminal gparted git
+sudo pacman -S obsidian tor torbrowser-launcher rnote flatpak sqlite zsh telegram-desktop git github-cli gnome-disk-utility binwalk firefox gnome-terminal gparted os-prober gdb gnome-boxes qt5 qt6 qtcreator
