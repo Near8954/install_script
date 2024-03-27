662ec6714697abd37ce5f69322747b606c870096
Near8954 <roma.baldin@gmail.com>
2024-03-27 13:03:04 +0300

    main.sh update

--- main.sh
+++ main.sh
@@ -4,9 +4,12 @@ chmod +x ./source/packages.sh
 chmod +x ./source/ohmyzsh.sh
 chmod +x ./source/snap.sh
 chmod +x ./source/yay.sh
+echo "Installing packages\n"
 ./source/packages.sh
+echo "Installing snap\n"
 ./source/snap.sh
+echo "Installing yay\n"
 ./source/yay.sh
+echo "Installing ohmyzsh\n"
 ./source/ohmyzsh.sh
-
-#reboot
+reboot
